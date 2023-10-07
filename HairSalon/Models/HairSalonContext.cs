1ef7903f94a655ee696e754aa0f418e3356c5a10
Brandon Wright <brandonfullstack@gmail.com>
2023-10-07 17:30:35 -0400

    Commit models

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+  public class HairSalonContext : DbContext
+  {
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Stylist> Stylists {get; set;}
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
+  }
+}
